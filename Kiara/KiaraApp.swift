4cb7a31c7f365227f3148e7291400bafce162083
Ludovic Fournier <fouludovic@gmail.com>
2023-01-31 23:14:19 -0500

    Initial Commit

---
+++ Kiara/KiaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  KiaraApp.swift
+//  Kiara
+//
+//  Created by Ludovic Fournier on 2023-01-31.
+//
+
+import SwiftUI
+
+@main
+struct KiaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
